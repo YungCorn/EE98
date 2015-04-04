b4e42aa02ad065115175d8d742eb491cb55e8967
Brad <bradleyfrizzell@gmail.com>
2015-04-04 17:57:05 -0400

    Made python program to generate XML files

---
+++ XMLGeneration/generateXML.py
@@ -0,0 +1,22 @@
+def generate():
+    f = open("calibPics.xml", 'w')
+    
+    start = ("<?xml version=\"1.0\"?>\
+             \n<opencv_storage>\
+             \n<imagelist>\n")
+    
+    f.write(start)
+    
+    num_pairs = 50
+    
+    for num in range(num_pairs):
+        line_left = ("\"raspPhotos/4_4_calib_photos/pics_4_4_left/left%d.png\"\n"%num)
+        line_right =("\"raspPhotos/4_4_calib_photos/pics_4_4_right/right%d.png\"\n"%num)
+        f.write(line_left)
+        f.write(line_right)
+        
+    end = ("</imagelist>\
+            \n</opencv_storage>")
+    f.write(end)
+    
+generate()
