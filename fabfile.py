276d9bf38dd13ab105df15c9f127c123f44ac320
Brad <bradleyfrizzell@gmail.com>
2015-04-05 18:56:04 -0400

    Added remote slave call

--- fabfile.py
+++ fabfile.py
@@ -10,10 +10,10 @@ from fabric.operations import get
 env.password = 'raspberry' # password of master
 env.user = 'pi'
 
-#def start_slave():
-    # run with 'fab -H <IPP ADDRESS OF SLAVE> start_slave'
-    #with cd('~/Documents/python/'):
-        #run('sudo python take_one_pic.py')
+def start_slave():
+    #run with 'fab -H <IPP ADDRESS OF SLAVE> start_slave'
+    with cd('~/Documents/python/'):
+        run('sudo python take_one_pic.py')
 
 
 def take_one_pic():
@@ -28,6 +28,12 @@ def get_master_pic():
 
 
 def get_slave_pic():
-    # run with 'fab -H <IP ADDRESS OF MASTER> get_master_pic'
+    # run with 'fab -H <IP ADDRESS OF SLAVE> get_master_pic'
     get('~/Documents/python/user_pic/right.png','user_pictures/right.png')
-        
+
+def kill_slave():
+    # run with 'fab -H <IP ADDRESS OF MASTER>  kill_slave' (this is not a mistake, you
+    # put the master's IP, not the slave's. The master kills the slave. It must be done
+    # this way
+    with cd('~/Documents/PythonTests/'):
+        run('sudo python kill_slave.py')
