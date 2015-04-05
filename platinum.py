034e3cf52643305aa991b91af689056f92757787
Brad <bradleyfrizzell@gmail.com>
2015-04-05 18:56:39 -0400

    Added comments

--- platinum.py
+++ platinum.py
@@ -11,15 +11,17 @@ def main():
     os.system("fab -H %s get_master_pic" % master_ip) # get photo from master
     os.system("fab -H %s get_slave_pic" % slave_ip) # get photo from slave
     
+    # Rectifies the photos
     os.system("./stereo_rect intrinsics.yml extrinsics.yml user_pictures/left.png \
                 user_pictures/right.png")
     
+    # This runs the GUI to display the pictures to the user for selection
     os.system("python pic_display.py")
+    
     
     with open("user_pictures_rect/coords.txt") as f:
         coords = f.readlines()
     c = coords[0].split()
-    
     os.system("./find_dist extrinsics.yml 1680 1050 {} {} {} {} {} {} {} {}".format(\
                 c[0], c[1], c[4], c[5], c[2], c[3], c[6], c[7]))
 
